0219e70ad6c114b289b13bc771807557f38de754
sandeepkaurs <sandeep.singh@telus.com>
2022-12-08 00:35:47 -0800

    Commit adding counter and red text if over 140 characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("Document's ready");
   // --- our code goes here ---
-  
+  // use input event, tweet-text is area where counter is
+  $("textarea").on("input", function() {
+    //max character count is 140
+    const maxCharacter = 140;
+    //const input character we can get using length
+    //using val(jquery) to get value of a textarea
+    let charCount = $(this).val().length;
+    //const counter should be max characters less inputted characters
+    let charactersLeft = maxCharacter - charCount;
+
+    let $counterButton = $(this).parent().find(".counter");
+    $counterButton.val(charactersLeft)
+
+    if (charactersLeft < 0) {
+      $counterButton.addClass("invalid");
+    } else {
+      $counterButton.removeClass("invalid");
+    }
+  });
 });
