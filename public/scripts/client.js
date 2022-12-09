66bb33c1adab9decfd888f0f6e651461fd4aecfb
sandeepkaurs <sandeep.singh@telus.com>
2022-12-09 09:57:06 -0800

    Adding changes made through several  assignments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,34 +6,19 @@
 
 // creating new tweet element
 $(document).ready(function () {
+  $("#error-message-noTweet").hide();
+  $("error-message-tooManyCharacters").hide();
+
+  const data = [];
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1670382605101
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1670469005101
-    }
-  ]
-
   // render tweets
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   const renderTweets = function(tweets) {
     // console.log(tweets)
     $('#tweets-container').empty();
@@ -43,23 +28,24 @@ $(document).ready(function () {
       $('#tweets-container').append($tweet)
     }
   };
+  // creating new tweets
   const newTweetElement = function (tweetData) {
     let $tweets = $(`
       <article class="tweet">
         <header class="tweet-header">
-          <div>
+          <div class="user-profile">
             <img class="user-icon" src=${tweetData.user.avatars}/>
             <span class="user-name">${tweetData.user.name}</span>
           </div>  
           <span class="user-handle">${tweetData.user.handle}</span>
         </header>
         <div class="tweet-text">
-          ${tweetData.content.text}
+          ${escape(tweetData.content.text)}
         </div>
         <footer class="tweet-footer">
-          <div class="tweetDate">
-            ${tweetData.created_at}
-          </div>
+          <span class="tweetDate">
+            ${timeago.format(tweetData.created_at)}
+          </span>
           <div class="responses">
             <i class="fa-solid fa-retweet"></i>
             <i class="fa-light fa-heart"></i>
@@ -70,11 +56,44 @@ $(document).ready(function () {
       </article> `);
     return $tweets
   };
-  
+  //making const loadTweets function
+  const loadTweets = function () {
+    $.get("/tweets/", function (newTweets) {
+      renderTweets(newTweets.reverse());
+    });
+  };
+  loadTweets();
+
+  // add a new tweet when clicking submit id
+  $("#new-tweet-form").submit(function (event) {
+    // add event.preventDefault to prevent default submission behaviour
+    event.preventDefault();
+    const maxCharacters = 140;
+    const inputValue = $(this).find("#tweet-text").val().length;
+
+    $("error-message-noTweet").slideUp("slow");
+    $("error-message-tooManyCharacters").slideUp("slow");
+
+    if (!inputValue) {
+      $("error-message-noTweet").slideDown("slow");
+      $("error-message-tooManyCharacters").hide();
+    } else if (inputValue - maxCharacters > 0) {
+      $("error-message-tooManyCharacters").slideDown("slow");
+      $("error-message-noTweet").hide();
+    } else {
+      const newTweets = $(this).serialize();
+      $.post("/tweets/", newTweets, () => {
+        $(this).find("#tweet-text").val("");
+        $(this).find(".counter").val(maxCharacters);
+        loadTweets();
+      });
+    }
+  });
+});
   // const $tweet = newTweetElement(tweetData);
   // console.log("hi there");
   // // Test / driver code (temporary)
   // console.log($tweet); // to see what it looks like
   // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  renderTweets(tweetData);
-});
+  //renderTweets(tweetData);
+
